91b49eb6f9d2a3afbd9099bda09fa3a74123464e
Benedikt Ritter <beneritter@gmail.com>
2023-09-21 21:14:01 +0200

    Manage git using home-manager

--- hosts/latitute-7280/configuration.nix
+++ hosts/latitute-7280/configuration.nix
@@ -119,6 +119,66 @@
         starship init fish | source
       '';
     };
+    programs.git = {
+      enable = true;
+
+      userName = "Benedikt Ritter";
+      userEmail = "beneritter@gmail.com";
+      signing = {
+        signByDefault = true;
+        key = "394546A47BB40E12";
+      };
+
+      includes = [
+        {
+          condition = "gitdir:~/github/gradlex-org";
+          contents = {
+            user.email = "benedikt@gradlex.org";
+            user.signingKey = "757DE51A2FD1489D";
+          };
+        }
+        {
+          condition = "gitdir:~/github/apache";
+          contents = {
+            user.email = "britter@apache.org";
+            user.signingKey = "9DAADC1C9FCC82D0";
+          };
+        }
+      ];
+
+      aliases = {
+        this = "!f() { git init && git add --all && git commit -m 'Initial commit'; }; f";
+        tags = "tag -l";
+        branches = "branch -a";
+        remotes = "remote -v";
+        lg = "log --all --graph --pretty=format:'%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+        cleanup = "!f() { git branch --merged main | grep -v main | xargs -n 1 git branch -D;  }; f";
+        cleanup-deleted = "!git remote prune origin && git branch -vv | grep ': gone]' | awk '{print $1}' | xargs git branch -D";
+        sync = "!f() { git checkout main && git fetch upstream && git merge upstream/main && git push origin main && git cleanup && git fetch -p;  }; f";
+        merge-back = "!f() { git fetch origin && git checkout release && git merge origin/release && git checkout main && git merge origin/main && git merge release && git cleanup-deleted;  }; f";
+        co = "checkout";
+        cm = "checkout main";
+        st = "status";
+        ci = "commit";
+        cia = "commit --amend";
+        rbi = "rebase --interactive main";
+        rbm = "rebase main";
+        rbc = "rebase --continue";
+      };
+
+      ignores = [
+        ## IntelliJ stuff
+        ".idea"
+        "*.iml"
+        "out/"
+
+        ## Mac OS stuff
+        ".DS_Store"
+      ];
+
+      diff-so-fancy.enable = true;
+    };
+    programs.gpg.enable = true;
   };
 
   nixpkgs.config.allowUnfreePredicate = pkg:
@@ -126,19 +186,17 @@
       "1password"
     ];
 
-  # List packages installed in system profile. To search, run:
+  # List packages installed in system profile. To searc, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     asdf-vm
     bat # cat replacement
     blackbox-terminal
-    diff-so-fancy
     exa # ls replacement
     fzf # fuzzy finding
     gh
-    git
     gradle
-    helix 
+    helix
     jetbrains.idea-community
     pinentry
     starship
@@ -157,7 +215,6 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent.enable = true;
   programs.fish.enable = true;
   programs._1password-gui.enable = true;
 
