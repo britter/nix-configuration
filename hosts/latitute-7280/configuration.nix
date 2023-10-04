5e56898c070580b82821ce0d66606ceb0b3c42e9
Benedikt Ritter <beneritter@gmail.com>
2023-10-04 20:11:10 +0200

    Manage gh cli including extension with home-manager

--- hosts/latitute-7280/configuration.nix
+++ hosts/latitute-7280/configuration.nix
@@ -216,6 +216,29 @@
       enable = true;
       pinentryFlavor = "gnome3";
     };
+    programs.gh = {
+      enable = true;
+
+      extensions =
+        let
+          gh-get = with pkgs; stdenv.mkDerivation rec {
+            name = "gh-get";
+            pname = "gh-get";
+            src = fetchFromGitHub {
+              owner = "britter";
+              repo = "gh-get";
+              rev = "v1.0.0";
+              sha256 = "sha256-2o7Ugi8Ba3rso68Onc8tuh/RzWxZ9OTkdJYgo3K6+Gs=";
+            };
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp gh-get $out/bin
+            '';
+            };
+        in
+        [ gh-get ];
+    };
 
     programs.java = {
       enable = true;
@@ -257,7 +280,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     blackbox-terminal
-    gh
     jetbrains.idea-community
     pinentry
     xsel # Access to X server clipboard, required for helix clipboard integration
