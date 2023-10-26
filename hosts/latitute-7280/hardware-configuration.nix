6997991cdca857fbae260aa50b60ce95ef0984ab
Benedikt Ritter <beneritter@gmail.com>
2023-10-26 19:52:52 +0200

    Set cpuFreqGovernor to ondemand
    This is better for performance which was pointed out
    by @Alper-Celik to me here:
    https://infosec.exchange/@Alper_Celik/111301697605535815

--- hosts/latitute-7280/hardware-configuration.nix
+++ hosts/latitute-7280/hardware-configuration.nix
@@ -33,6 +33,6 @@
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
