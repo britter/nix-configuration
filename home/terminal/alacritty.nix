dca81f45487286bcd6e416fcde25c1decb3a2227
Benedikt Ritter <beneritter@gmail.com>
2023-12-01 17:15:44 +0100

    Always start alacritty with a tmux session

--- home/terminal/alacritty.nix
+++ home/terminal/alacritty.nix
@@ -3,6 +3,10 @@
     enable = true;
     settings = {
       font.normal.family = "FiraCode Nerd Font";
+      shell = {
+        program = "${pkgs.tmux}/bin/tmux";
+        args =[ "new-session" "-A" "-D" "-s" "default"];
+      };
     };
   };
 }
