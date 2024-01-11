e60aea9f732029f1f4abd80377f40eb28637b99b
Benedikt Ritter <beneritter@gmail.com>
2024-01-11 16:48:53 +0100

    Pass custom versions of baresip and libre to overlay-vst

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,11 @@
 with pkgs; rec {
   gh-get = callPackage ./gh-get {};
   groovy-language-server = callPackage ./groovy-language-server {};
-  overlay-vst = callPackage ./overlay-vst {inherit baresip-studio-link vst-sdk;};
+  overlay-vst = callPackage ./overlay-vst {
+    inherit vst-sdk;
+    baresip = studio-link-baresip;
+    libre = studio-link-libre;
+  };
   studio-link-baresip = callPackage ./studio-link-baresip {libre = studio-link-libre;};
   studio-link-libre = callPackage ./studio-link-libre {};
   vst-sdk = callPackage ./vst-sdk {};
