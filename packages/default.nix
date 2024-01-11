a7a9c0ee7269c5ab07057077efa55ece2d6f66e3
Benedikt Ritter <beneritter@gmail.com>
2024-01-11 16:44:56 +0100

    Use appropriate custom version of libre to build baresip
    Now it's failing because of librem, so we need to fix that next.

--- packages/default.nix
+++ packages/default.nix
@@ -1,8 +1,9 @@
 {pkgs, ...}:
 with pkgs; rec {
-  baresip-studio-link = callPackage ./baresip-studio-link {};
+  baresip-studio-link = callPackage ./baresip-studio-link {libre = studio-link-libre;};
   gh-get = callPackage ./gh-get {};
   groovy-language-server = callPackage ./groovy-language-server {};
   overlay-vst = callPackage ./overlay-vst {inherit baresip-studio-link vst-sdk;};
+  studio-link-libre = callPackage ./studio-link-libre {};
   vst-sdk = callPackage ./vst-sdk {};
 }
