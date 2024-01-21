a4aeac1c81b204b3b48824672c361e61b8e5437e
Benedikt Ritter <beneritter@gmail.com>
2024-01-21 16:15:41 +0100

    Build baresip flavors and pass them to the right place

--- packages/default.nix
+++ packages/default.nix
@@ -1,24 +1,33 @@
-{pkgs, ...}:
-with pkgs; rec {
-  gh-get = callPackage ./gh-get {};
-  groovy-language-server = callPackage ./groovy-language-server {};
-  opus = callPackage ./opus {};
-  overlay-onair-lv2 = callPackage ./overlay-onair-lv2 {
-    inherit opus;
-    baresip = studio-link-baresip;
-    libre = studio-link-libre;
-  };
-  overlay-vst = callPackage ./overlay-vst {
-    inherit vst-sdk opus;
-    baresip = studio-link-baresip;
-    libre = studio-link-libre;
-  };
-  studio-link-baresip = callPackage ./studio-link-baresip {
-    inherit opus;
-    libre = studio-link-libre;
-    librem = studio-link-librem;
-  };
-  studio-link-libre = callPackage ./studio-link-libre {};
-  studio-link-librem = callPackage ./studio-link-librem {libre = studio-link-libre;};
-  vst-sdk = callPackage ./vst-sdk {};
-}
+{pkgs, ...}: let
+  baresip = import ./studio-link-baresip;
+in
+  with pkgs; rec {
+    gh-get = callPackage ./gh-get {};
+    groovy-language-server = callPackage ./groovy-language-server {};
+    opus = callPackage ./opus {};
+    overlay-onair-lv2 = callPackage ./overlay-onair-lv2 {
+      inherit opus;
+      baresip = studio-link-baresip-effectonair-plugin;
+      libre = studio-link-libre;
+    };
+    overlay-vst = callPackage ./overlay-vst {
+      inherit vst-sdk opus;
+      baresip = studio-link-baresip-effect-plugin;
+      libre = studio-link-libre;
+    };
+    studio-link-baresip-effect-plugin =
+      callPackage baresip.effect-plugin
+      {
+        inherit opus;
+        libre = studio-link-libre;
+        librem = studio-link-librem;
+      };
+    studio-link-baresip-effectonair-plugin = callPackage baresip.effectonair-plugin {
+      inherit opus;
+      libre = studio-link-libre;
+      librem = studio-link-librem;
+    };
+    studio-link-libre = callPackage ./studio-link-libre {};
+    studio-link-librem = callPackage ./studio-link-librem {libre = studio-link-libre;};
+    vst-sdk = callPackage ./vst-sdk {};
+  }
--- packages/studio-link-baresip/default.nix
+++ packages/studio-link-baresip/default.nix
@@ -1,147 +1,156 @@
-{
-  lib,
-  stdenv,
-  fetchzip,
-  fetchFromGitHub,
-  zlib,
-  openssl,
-  libre,
-  librem,
-  pkg-config,
-  gst_all_1,
-  cairo,
-  mpg123,
-  alsa-lib,
-  SDL2,
-  libv4l,
-  celt,
-  libsndfile,
-  srtp,
-  ffmpeg,
-  gsm,
-  speex,
-  portaudio,
-  spandsp3,
-  libuuid,
-  libvpx,
-  opus,
-  flac,
-}: let
-  studio-link-app = fetchFromGitHub {
-    owner = "Studio-Link";
-    repo = "app";
-    rev = "v21.07.0-stable";
-    sha256 = "sha256-BjMZQDweQMM2TZuYoAGWE9eC/Vy8UnwDkkieY9TKpyg=";
-  };
-  # See https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L37-L43
-  studio-link-webui = fetchzip {
-    url = "https://download.studio.link/devel/v21.xx.x/v21.12.0-beta-3bb3eaf/webui.zip";
-    sha256 = "sha256-7SkMQ8WM7vgFuiKS8+BFat4M/a9VNrWQKLMynKE4sEU=";
-  };
-in
-  stdenv.mkDerivation (finalAttrs: {
-    pname = "baresip-studio-link";
-    version = "1.0.0";
-    src = fetchzip {
-      url = "https://github.com/baresip/baresip/archive/v${finalAttrs.version}.tar.gz";
-      sha256 = "sha256-HCnLidhuLP1QzFJ93jTk/+N86S+qZKnvtFHUAyUXPRM=";
+rec {
+  flavor = {modules}: {
+    lib,
+    stdenv,
+    fetchzip,
+    fetchFromGitHub,
+    zlib,
+    openssl,
+    libre,
+    librem,
+    pkg-config,
+    gst_all_1,
+    cairo,
+    mpg123,
+    alsa-lib,
+    SDL2,
+    libv4l,
+    celt,
+    libsndfile,
+    srtp,
+    ffmpeg,
+    gsm,
+    speex,
+    portaudio,
+    spandsp3,
+    libuuid,
+    libvpx,
+    opus,
+    flac,
+  }: let
+    studio-link-app = fetchFromGitHub {
+      owner = "Studio-Link";
+      repo = "app";
+      rev = "v21.07.0-stable";
+      sha256 = "sha256-BjMZQDweQMM2TZuYoAGWE9eC/Vy8UnwDkkieY9TKpyg=";
     };
+    # See https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L37-L43
+    studio-link-webui = fetchzip {
+      url = "https://download.studio.link/devel/v21.xx.x/v21.12.0-beta-3bb3eaf/webui.zip";
+      sha256 = "sha256-7SkMQ8WM7vgFuiKS8+BFat4M/a9VNrWQKLMynKE4sEU=";
+    };
+  in
+    stdenv.mkDerivation (finalAttrs: {
+      pname = "baresip-studio-link";
+      version = "1.0.0";
+      src = fetchzip {
+        url = "https://github.com/baresip/baresip/archive/v${finalAttrs.version}.tar.gz";
+        sha256 = "sha256-HCnLidhuLP1QzFJ93jTk/+N86S+qZKnvtFHUAyUXPRM=";
+      };
 
-    # Link custom modules, see https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L123-L131
-    postUnpack = ''
-      rm -rf $sourceRoot/modules/g722
-      ln -s ${studio-link-app}/src/modules/g722 $sourceRoot/modules/g722
-      ln -s ${studio-link-app}/src/modules/slogging $sourceRoot/modules/slogging
-      ln -s ${studio-link-app}/src/modules/effect $sourceRoot/modules/effect
-      ln -s ${studio-link-app}/src/modules/effectonair $sourceRoot/modules/effectonair
-      ln -s ${studio-link-app}/src/modules/apponair $sourceRoot/modules/apponair
-      ln -s ${studio-link-app}/src/modules/slaudio $sourceRoot/modules/slaudio
+      # Link custom modules, see https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L123-L131
+      postUnpack = ''
+        rm -rf $sourceRoot/modules/g722
+        ln -s ${studio-link-app}/src/modules/g722 $sourceRoot/modules/g722
+        ln -s ${studio-link-app}/src/modules/slogging $sourceRoot/modules/slogging
+        ln -s ${studio-link-app}/src/modules/effect $sourceRoot/modules/effect
+        ln -s ${studio-link-app}/src/modules/effectonair $sourceRoot/modules/effectonair
+        ln -s ${studio-link-app}/src/modules/apponair $sourceRoot/modules/apponair
+        ln -s ${studio-link-app}/src/modules/slaudio $sourceRoot/modules/slaudio
 
-      mkdir -p $sourceRoot/modules/webapp/assets
-      cp -r ${studio-link-app}/src/modules/webapp/* $sourceRoot/modules/webapp/
-      cp ${studio-link-webui}/headers/* $sourceRoot/modules/webapp/assets/
-    '';
+        mkdir -p $sourceRoot/modules/webapp/assets
+        cp -r ${studio-link-app}/src/modules/webapp/* $sourceRoot/modules/webapp/
+        cp ${studio-link-webui}/headers/* $sourceRoot/modules/webapp/assets/
+      '';
 
-    # Patches are defined here: https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L110-L115
-    patches = [
-      "${studio-link-app}/dist/patches/config.patch"
-      "${studio-link-app}/dist/patches/fix_check_telev_and_pthread.patch"
-      "${studio-link-app}/dist/patches/dtls_aes256.patch"
-      "${studio-link-app}/dist/patches/rtcp_mux_softphone.patch"
-      "${studio-link-app}/dist/patches/fallback_dns.patch"
-      "${studio-link-app}/dist/patches/baresip_audio_rtp_discard.patch"
-    ];
+      # Patches are defined here: https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L110-L115
+      patches = [
+        "${studio-link-app}/dist/patches/config.patch"
+        "${studio-link-app}/dist/patches/fix_check_telev_and_pthread.patch"
+        "${studio-link-app}/dist/patches/dtls_aes256.patch"
+        "${studio-link-app}/dist/patches/rtcp_mux_softphone.patch"
+        "${studio-link-app}/dist/patches/fallback_dns.patch"
+        "${studio-link-app}/dist/patches/baresip_audio_rtp_discard.patch"
+      ];
 
-    nativeBuildInputs = [pkg-config];
+      nativeBuildInputs = [pkg-config];
 
-    buildInputs =
-      [
-        zlib
-        openssl
-        libre
-        librem
-        cairo
-        mpg123
-        alsa-lib
-        SDL2
-        libv4l
-        celt
-        libsndfile
-        srtp
-        ffmpeg
-        gsm
-        speex
-        portaudio
-        spandsp3
-        libuuid
-        libvpx
-        opus
-        flac
-      ]
-      ++ (with gst_all_1; [gstreamer gst-libav gst-plugins-base gst-plugins-bad gst-plugins-good]);
+      buildInputs =
+        [
+          zlib
+          openssl
+          libre
+          librem
+          cairo
+          mpg123
+          alsa-lib
+          SDL2
+          libv4l
+          celt
+          libsndfile
+          srtp
+          ffmpeg
+          gsm
+          speex
+          portaudio
+          spandsp3
+          libuuid
+          libvpx
+          opus
+          flac
+        ]
+        ++ (with gst_all_1; [gstreamer gst-libav gst-plugins-base gst-plugins-bad gst-plugins-good]);
 
-    # https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L123C18-L123C97
-    preBuild = ''
-      makeFlagsArray+=(MODULES="opus stdio ice g711 turn stun uuid auloop webapp effect g722 slogging dtls_srtp")
-    '';
+      # https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L123C18-L123C97
+      preBuild = ''
+        makeFlagsArray+=(MODULES="opus stdio ice g711 turn stun uuid auloop g722 slogging dtls_srtp ${modules}")
+      '';
 
-    makeFlags =
-      [
-        "LIBRE_MK=${libre}/share/re/re.mk"
-        "LIBRE_INC=${libre}/include/re"
-        "LIBRE_SO=${libre}/lib"
-        "LIBREM_PATH=${librem}"
-        "PREFIX=$(out)"
-        "USE_VIDEO=1"
-        "CCACHE_DISABLE=1"
+      makeFlags =
+        [
+          "LIBRE_MK=${libre}/share/re/re.mk"
+          "LIBRE_INC=${libre}/include/re"
+          "LIBRE_SO=${libre}/lib"
+          "LIBREM_PATH=${librem}"
+          "PREFIX=$(out)"
+          "USE_VIDEO=1"
+          "CCACHE_DISABLE=1"
 
-        # https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L122
-        "STATIC=1"
-        # https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L125
-        "libbaresip.a"
-      ]
-      ++ lib.optional (stdenv.cc.cc != null) "SYSROOT_ALT=${stdenv.cc.cc}"
-      ++ lib.optional (stdenv.cc.libc != null) "SYSROOT=${stdenv.cc.libc}";
+          # https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L122
+          "STATIC=1"
+          # https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L125
+          "libbaresip.a"
+        ]
+        ++ lib.optional (stdenv.cc.cc != null) "SYSROOT_ALT=${stdenv.cc.cc}"
+        ++ lib.optional (stdenv.cc.libc != null) "SYSROOT=${stdenv.cc.libc}";
 
-    # -DSLPLUGIN coming from https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L124C40-L124C50
-    # Include of webapp coming from https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L136
-    NIX_CFLAGS_COMPILE = ''
-      -I${librem}/include/rem
-      -I${gsm}/include/gsm
-      -I${studio-link-app}/src/modules/webapp
-      -DHAVE_INTTYPES_H
-      -D__GLIBC__
-      -D__need_timeval
-      -D__need_timespec
-      -D__need_time_t
-      -DSLPLUGIN
-    '';
+      # -DSLPLUGIN coming from https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/build.sh#L124C40-L124C50
+      # Include of webapp coming from https://github.com/Studio-Link/app/blob/v21.07.0-stable/dist/lib/functions.sh#L136
+      NIX_CFLAGS_COMPILE = ''
+        -I${librem}/include/rem
+        -I${gsm}/include/gsm
+        -I${studio-link-app}/src/modules/webapp
+        -DHAVE_INTTYPES_H
+        -D__GLIBC__
+        -D__need_timeval
+        -D__need_timespec
+        -D__need_time_t
+        -DSLPLUGIN
+      '';
 
-    postInstall = ''
-      mkdir -p $out/include
-      cp include/* $out/include/
+      postInstall = ''
+        mkdir -p $out/include
+        cp include/* $out/include/
 
-      mkdir -p $out/lib
-      cp libbaresip.a $out/lib
-    '';
-  })
+        mkdir -p $out/lib
+        cp libbaresip.a $out/lib
+      '';
+    });
+
+  effect-plugin = flavor {
+    modules = "webapp effect";
+  };
+  effectonair-plugin = flavor {
+    modules = "apponair effectonair";
+  };
+}
