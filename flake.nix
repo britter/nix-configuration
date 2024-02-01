a13392cd1909c2bfd55776b4bbfec9d72e621835
Benedikt Ritter <beneritter@gmail.com>
2024-02-01 20:55:48 +0100

    flake: use flake-utils.lib.system to define systems

--- flake.nix
+++ flake.nix
@@ -35,64 +35,73 @@
       packages = import ./packages {inherit pkgs;};
     })
     // {
-      nixosConfigurations.latitue-7280 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          {
-            nixpkgs.overlays = [
-              (final: prev: {
-                jdt-language-server = nixpkgs-unstable.legacyPackages.x86_64-linux.jdt-language-server;
-              })
-            ];
-          }
-          ./hosts/latitute-7280/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.bene = {
-              home.stateVersion = "23.05";
-              imports = [./home.nix];
-            };
-          }
-        ];
-      };
-      nixosConfigurations.pi-hole = nixpkgs.lib.nixosSystem {
-        system = "aarch64-linux";
-        modules = [
-          ./hosts/pi-hole/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.nixos = {
-              home.stateVersion = "23.05";
-              imports = [./pi-home.nix];
-            };
-          }
-        ];
-      };
-      darwinConfigurations.WQ0C6FWJ1W = nix-darwin.lib.darwinSystem {
-        system = "aarch64_darwin";
-        modules = [
-          ./hosts/work-macbook/configuration.nix
-          {
-            nixpkgs.overlays = [
-              (final: prev: {
-                jdt-language-server = nixpkgs-unstable.legacyPackages.aarch64-darwin.jdt-language-server;
-              })
-            ];
-          }
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.bene = {
-              home.stateVersion = "23.05";
-              imports = [./work-home.nix];
-            };
-          }
-        ];
-      };
+      nixosConfigurations.latitue-7280 = let
+        system = flake-utils.lib.system.x86_64-linux;
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            {
+              nixpkgs.overlays = [
+                (final: prev: {
+                  jdt-language-server = nixpkgs-unstable.legacyPackages.${system}.jdt-language-server;
+                })
+              ];
+            }
+            ./hosts/latitute-7280/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.bene = {
+                home.stateVersion = "23.05";
+                imports = [./home.nix];
+              };
+            }
+          ];
+        };
+      nixosConfigurations.pi-hole = let
+        system = flake-utils.lib.system.aarch64-linux;
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./hosts/pi-hole/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.nixos = {
+                home.stateVersion = "23.05";
+                imports = [./pi-home.nix];
+              };
+            }
+          ];
+        };
+      darwinConfigurations.WQ0C6FWJ1W = let
+        system = flake-utils.lib.system.aarch64-darwin;
+      in
+        nix-darwin.lib.darwinSystem {
+          inherit system;
+          modules = [
+            ./hosts/work-macbook/configuration.nix
+            {
+              nixpkgs.overlays = [
+                (final: prev: {
+                  jdt-language-server = nixpkgs-unstable.legacyPackages.${system}.jdt-language-server;
+                })
+              ];
+            }
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.bene = {
+                home.stateVersion = "23.05";
+                imports = [./work-home.nix];
+              };
+            }
+          ];
+        };
     };
 }
