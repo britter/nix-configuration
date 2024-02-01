0f64eb5922f334c3b869181f5ea5d8ba7e365f86
Benedikt Ritter <beneritter@gmail.com>
2024-02-01 20:42:27 +0100

    flake: introduce flake-utils
    This makes it easier to define formatter, packages, etc. for
    all default systems.

--- flake.nix
+++ flake.nix
@@ -15,6 +15,8 @@
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
@@ -23,71 +25,73 @@
     nixpkgs-unstable,
     home-manager,
     nix-darwin,
+    flake-utils,
     ...
-  }: {
-    formatter = {
-      x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-      aarch64-linux = nixpkgs.legacyPackages.aarch64-linux.alejandra;
-      aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
+    })
+    // {
+      nixosConfigurations.latitue-7280 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          {
+            nixpkgs.overlays = [
+              (final: prev: {
+                jdt-language-server = nixpkgs-unstable.legacyPackages.x86_64-linux.jdt-language-server;
+              })
+            ];
+          }
+          ./hosts/latitute-7280/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.bene = {
+              home.stateVersion = "23.05";
+              imports = [./home.nix];
+            };
+          }
+        ];
+      };
+      nixosConfigurations.pi-hole = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          ./hosts/pi-hole/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.nixos = {
+              home.stateVersion = "23.05";
+              imports = [./pi-home.nix];
+            };
+          }
+        ];
+      };
+      darwinConfigurations.WQ0C6FWJ1W = nix-darwin.lib.darwinSystem {
+        system = "aarch64_darwin";
+        modules = [
+          ./hosts/work-macbook/configuration.nix
+          {
+            nixpkgs.overlays = [
+              (final: prev: {
+                jdt-language-server = nixpkgs-unstable.legacyPackages.aarch64-darwin.jdt-language-server;
+              })
+            ];
+          }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.bene = {
+              home.stateVersion = "23.05";
+              imports = [./work-home.nix];
+            };
+          }
+        ];
+      };
     };
-    nixosConfigurations.latitue-7280 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        {
-          nixpkgs.overlays = [
-            (final: prev: {
-              jdt-language-server = nixpkgs-unstable.legacyPackages.x86_64-linux.jdt-language-server;
-            })
-          ];
-        }
-        ./hosts/latitute-7280/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.bene = {
-            home.stateVersion = "23.05";
-            imports = [./home.nix];
-          };
-        }
-      ];
-    };
-    nixosConfigurations.pi-hole = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        ./hosts/pi-hole/configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.nixos = {
-            home.stateVersion = "23.05";
-            imports = [./pi-home.nix];
-          };
-        }
-      ];
-    };
-    darwinConfigurations.WQ0C6FWJ1W = nix-darwin.lib.darwinSystem {
-      system = "aarch64_darwin";
-      modules = [
-        ./hosts/work-macbook/configuration.nix
-        {
-          nixpkgs.overlays = [
-            (final: prev: {
-              jdt-language-server = nixpkgs-unstable.legacyPackages.aarch64-darwin.jdt-language-server;
-            })
-          ];
-        }
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.bene = {
-            home.stateVersion = "23.05";
-            imports = [./work-home.nix];
-          };
-        }
-      ];
-    };
-  };
 }
