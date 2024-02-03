6ee3e6cd7a04e6c10fec2581d6baf46dfe9d07f1
Benedikt Ritter <beneritter@gmail.com>
2024-02-03 16:34:03 +0100

    helix: configure groovy language
    Part of #4

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -1,4 +1,11 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  nvim-treesitter = pkgs.fetchFromGitHub {
+    owner = "nvim-treesitter";
+    repo = "nvm-treesitter";
+    rev = "f75a5b4e144228e4b2ab3006ed167b6fe37d0b33";
+    sha256 = "sha256-I5Dc80BgyCdnN3nMzCFEURsQL/gaudgJLfjH+HWpK+s=";
+  };
+in {
   programs.helix = {
     enable = true;
     defaultEditor = true;
@@ -26,5 +33,37 @@
         pagedown = "no_op";
       };
     };
+    languages = {
+      language-server.groovy = {
+        command = "${pkgs.groovy-language-server}/bin/groovy-language-server";
+      };
+      language = [
+        {
+          name = "groovy";
+          language-id = "groovy";
+          scope = "source.groovy";
+          injection-regex = "groovy";
+          file-types = ["groovy" "gradle" "Jenkinsfile"];
+          shebangs = ["groovy"];
+          roots = ["build.gradle" "build.gradle.kts" "settings.gradle" "settings.gradle.kts" "pom.xml"];
+          comment-token = "//";
+          indent = {
+            tab-width = 2;
+            unit = "  ";
+          };
+          language-servers = ["groovy"];
+        }
+      ];
+      grammar = [
+        {
+          name = "groovy";
+          source = {
+            git = "https://github.com/murtaza64/tree-sitter-groovy";
+            rev = "235009aad0f580211fc12014bb0846c3910130c1";
+          };
+        }
+      ];
+    };
   };
+  xdg.configFile."helix/runtime/queries/groovy".source = "${nvim-treesitter}/queries/groovy";
 }
