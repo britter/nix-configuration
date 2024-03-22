4865abc15b981ed250dc7872224f32305f6f7d63
Benedikt Ritter <beneritter@gmail.com>
2024-03-22 17:26:11 +0100

    home (alacritty): Use string literal instead of URL literal

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   programs.alacritty = let
     catpuccin-frappe = pkgs.fetchurl {
-      url = https://raw.githubusercontent.com/catppuccin/alacritty/main/catppuccin-mocha.yml;
+      url = "https://raw.githubusercontent.com/catppuccin/alacritty/main/catppuccin-mocha.yml";
       hash = "sha256-28Tvtf8A/rx40J9PKXH6NL3h/OKfn3TQT1K9G8iWCkM=";
     };
   in {
