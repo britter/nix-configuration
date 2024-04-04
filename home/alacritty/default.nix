7cf2fc4bd169a7bfd805ff75b544568a14239ccf
Benedikt Ritter <beneritter@gmail.com>
2024-04-04 14:26:08 +0200

    home (alacritty): Pin theme file
    Previously the code would fetch the catppuccin-mocha.yml file
    from the latest commit of the GitHub repository. In the
    meantime the repository was changed to contain the thema as
    a toml file instead. This made the rebuild fail when I
    reinstalled on latitude-7280.
    
    The configuration now pins the exact revision so the yml version
    of the file is guaranteed to be present.

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -1,13 +1,15 @@
 {pkgs, ...}: {
   programs.alacritty = let
-    catpuccin-mocha = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/alacritty/main/catppuccin-mocha.yml";
-      hash = "sha256-28Tvtf8A/rx40J9PKXH6NL3h/OKfn3TQT1K9G8iWCkM=";
+    catpuccin-alacritty = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "alacritty";
+      rev = "3c808cbb4f9c87be43ba5241bc57373c793d2f17";
+      hash = "sha256-w9XVtEe7TqzxxGUCDUR9BFkzLZjG8XrplXJ3lX6f+x0=";
     };
   in {
     enable = true;
     settings = {
-      import = [catpuccin-mocha];
+      import = ["${catpuccin-alacritty}/catppuccin-mocha.yml"];
       font.normal.family = "FiraCode Nerd Font";
       shell = {
         program = "${pkgs.tmux}/bin/tmux";
