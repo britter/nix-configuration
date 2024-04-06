34628ff55a1830dbe01ddd065c51ce067acecb9b
Benedikt Ritter <beneritter@gmail.com>
2024-04-06 20:59:07 +0200

    hosts (pulse-14): Format configuration files

--- hosts/pulse-14/configuration.nix
+++ hosts/pulse-14/configuration.nix
@@ -1,18 +1,19 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
     ../../modules/common-utilities
     ../../modules/nix
     ../../modules/gnome
     ../../modules/1password
-    ];
+  ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -74,7 +75,7 @@
   users.users.bene = {
     isNormalUser = true;
     description = "Benedikt Ritter";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = ["networkmanager" "wheel"];
     shell = pkgs.fish;
   };
 
@@ -91,5 +92,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
