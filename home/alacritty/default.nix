c5c8444fad2491068ad6a5dff70425d973cfe154
Benedikt Ritter <beneritter@gmail.com>
2024-04-18 13:29:08 +0200

    home (alacritty): tweak window look a little
    Make the window opaque a little bit and add some padding.
    Unfortunately there is a problem with the current setup:
    
    helix is not opaque for some reason, see https://github.com/helix-editor/helix/issues/4740

--- home/alacritty/default.nix
+++ home/alacritty/default.nix
@@ -11,6 +11,14 @@
         program = "${pkgs.tmux}/bin/tmux";
         args = ["new-session" "-A" "-D" "-s" "default"];
       };
+      window = {
+        padding = {
+          x = 20;
+          y = 20;
+        };
+        opacity = 0.85;
+        decorations = "None";
+      };
     };
   };
 }
