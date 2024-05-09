efe63f80ad6004b9635b447118038e6dab854384
Benedikt Ritter <beneritter@gmail.com>
2024-05-09 20:20:34 +0200

    hosts (pulse-14): Enable amdgpu drivers on x11

--- hosts/pulse-14/configuration.nix
+++ hosts/pulse-14/configuration.nix
@@ -35,6 +35,8 @@
 
   services.printing.enable = true;
 
+  services.xserver.videoDrivers = ["amdgpu"];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
