da92bc8c7701ba546c67e3c0d9bd9b1227e37c78
Benedikt Ritter <beneritter@gmail.com>
2024-05-31 16:21:42 +0200

    isos (minimal-server): format code

--- isos/minimal-server-iso/configuration.nix
+++ isos/minimal-server-iso/configuration.nix
@@ -21,10 +21,12 @@
 
   networking = {
     usePredictableInterfaceNames = false;
-    interfaces.eth0.ipv4.addresses = [{
-      address = "192.168.178.199";
-      prefixLength = 24;
-    }];
+    interfaces.eth0.ipv4.addresses = [
+      {
+        address = "192.168.178.199";
+        prefixLength = 24;
+      }
+    ];
     defaultGateway = "192.168.178.1";
   };
 }
