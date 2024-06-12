56e214687017be1d494d49b5a1f3a68ce18f4db0
Benedikt Ritter <beneritter@gmail.com>
2024-06-12 11:23:58 +0200

    Use version 1.4.7

--- home/terminal/terraform.nix
+++ home/terminal/terraform.nix
@@ -13,7 +13,7 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages =
       lib.optionals config.my.home.profiles.private.enable [pkgs.terraform]
-      ++ lib.optionals config.my.home.profiles.work.enable [pkgs.terraform-versions."1.4.6"];
+      ++ lib.optionals config.my.home.profiles.work.enable [pkgs.terraform-versions."1.4.7"];
 
     programs.helix.extraPackages = with pkgs; [
       terraform-ls
