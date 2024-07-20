21f1d80c148a488a0a1583e21c58c20703135eb7
Benedikt Ritter <beneritter@gmail.com>
2024-07-20 20:41:12 +0200

    home (sway): Remove default shotcuts
    Also configure move container to workspace to switch
    to that workspace at the same time.

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -27,12 +27,6 @@ in {
         menu = "${lib.getExe config.programs.rofi.finalPackage} -show drun -show-icons -pid";
         bars = [{command = "${pkgs.waybar}/bin/waybar";}];
 
-        modifier = "Mod4";
-        left = "h";
-        down = "j";
-        up = "k";
-        right = "l";
-
         input = {
           "type:touchpad" = {
             tap = "enabled";
@@ -47,65 +41,25 @@ in {
           "*".bg = "${../desktop/wallpapers/moebius-wallpaper-light.png} fill";
         };
 
+        modifier = "Mod4";
         keybindings = let
-          swayCfg = config.wayland.windowManager.sway.config;
-          mod = swayCfg.modifier;
-          inherit
-            (swayCfg)
-            terminal
-            menu
-            left
-            down
-            up
-            right
-            ;
-        in {
-          "${mod}+Return" = "exec ${terminal}";
-          "${mod}+Shift+q" = "kill";
-          "${mod}+d" = "exec ${menu}";
+          mod = config.wayland.windowManager.sway.config.modifier;
+        in
+          lib.mkOptionDefault {
+            # for defaults, see https://github.com/nix-community/home-manager/blob/master/modules/services/window-managers/i3-sway/sway.nix
 
-          "${mod}+${left}" = "focus left";
-          "${mod}+${down}" = "focus down";
-          "${mod}+${up}" = "focus up";
-          "${mod}+${right}" = "focus right";
-
-          "${mod}+Left" = "focus left";
-          "${mod}+Down" = "focus down";
-          "${mod}+Up" = "focus up";
-          "${mod}+Right" = "focus right";
-
-          "${mod}+Shift+${left}" = "move left";
-          "${mod}+Shift+${down}" = "move down";
-          "${mod}+Shift+${up}" = "move up";
-          "${mod}+Shift+${right}" = "move right";
-
-          "${mod}+Shift+Left" = "move left";
-          "${mod}+Shift+Down" = "move down";
-          "${mod}+Shift+Up" = "move up";
-          "${mod}+Shift+Right" = "move right";
-
-          "${mod}+1" = "workspace number 1";
-          "${mod}+2" = "workspace number 2";
-          "${mod}+3" = "workspace number 3";
-          "${mod}+4" = "workspace number 4";
-          "${mod}+5" = "workspace number 5";
-          "${mod}+6" = "workspace number 6";
-          "${mod}+7" = "workspace number 7";
-          "${mod}+8" = "workspace number 8";
-          "${mod}+9" = "workspace number 9";
-          "${mod}+0" = "workspace number 10";
-
-          "${mod}+Shift+1" = "move container to workspace number 1";
-          "${mod}+Shift+2" = "move container to workspace number 2";
-          "${mod}+Shift+3" = "move container to workspace number 3";
-          "${mod}+Shift+4" = "move container to workspace number 4";
-          "${mod}+Shift+5" = "move container to workspace number 5";
-          "${mod}+Shift+6" = "move container to workspace number 6";
-          "${mod}+Shift+7" = "move container to workspace number 7";
-          "${mod}+Shift+8" = "move container to workspace number 8";
-          "${mod}+Shift+9" = "move container to workspace number 9";
-          "${mod}+Shift+0" = "move container to workspace number 10";
-        };
+            # Override moving containers to also switch to that workspace
+            "${mod}+Shift+1" = "move container to workspace number 1; workspace number 1";
+            "${mod}+Shift+2" = "move container to workspace number 2; workspace number 2";
+            "${mod}+Shift+3" = "move container to workspace number 3; workspace number 3";
+            "${mod}+Shift+4" = "move container to workspace number 4; workspace number 4";
+            "${mod}+Shift+5" = "move container to workspace number 5; workspace number 5";
+            "${mod}+Shift+6" = "move container to workspace number 6; workspace number 6";
+            "${mod}+Shift+7" = "move container to workspace number 7; workspace number 7";
+            "${mod}+Shift+8" = "move container to workspace number 8; workspace number 8";
+            "${mod}+Shift+9" = "move container to workspace number 9; workspace number 9";
+            "${mod}+Shift+0" = "move container to workspace number 10; workspace number 10";
+          };
       };
     };
 
