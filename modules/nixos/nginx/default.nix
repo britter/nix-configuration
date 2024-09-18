c126ce501c781b00555f5333b653444d618fb36e
Benedikt Ritter <beneritter@gmail.com>
2024-09-18 14:33:53 +0200

    modules (nginx): Add defaulting for SSL connections.

--- modules/nixos/nginx/default.nix
+++ modules/nixos/nginx/default.nix
@@ -2,6 +2,26 @@ _: {
   services.nginx.virtualHosts.default = {
     serverName = "_";
     default = true;
+    listen = [
+      {
+        addr = "0.0.0.0";
+        port = 80;
+      }
+      {
+        addr = "[::0]";
+        port = 80;
+      }
+      {
+        addr = "0.0.0.0";
+        port = 443;
+        ssl = true;
+      }
+      {
+        addr = "[::0]";
+        port = 443;
+        ssl = true;
+      }
+    ];
     locations."/" = {
       return = 404;
     };
