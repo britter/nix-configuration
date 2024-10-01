a0363a05b8e83777a8a44cd216fe2758c0185e83
Benedikt Ritter <beneritter@gmail.com>
2024-10-01 11:03:55 +0200

    home (terminal): Add pdf-fax application
    pdf-fax is a shell application that uses Image Magick to make a PDF file
    look like a fax or badly scanned in page.
    
    This is inspired by the Babashka script posted by @n2o as a gist here:
    https://gist.github.com/n2o/c3f9df1343d0928303612af2005a23f7

--- home/terminal/tools.nix
+++ home/terminal/tools.nix
@@ -9,22 +9,39 @@ in {
   options.my.home.terminal.tools = {
     enable = lib.mkEnableOption "tools";
   };
-  config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      curl
-      eza # ls replacement
-      file
-      httpie
-      jq
-      mob # smooth git handover
-      tokei # count lines of code
-      tldr # better man pages
-      unzip
-      wget
-      zip
-    ];
-    programs.fzf.enable = true; # fuzzy finding
-    programs.ripgrep.enable = true; # recursive grep
-    programs.zoxide.enable = true; # smart cd replacement
-  };
+  config = let
+    fax-pdf = pkgs.writeShellApplication {
+      name = "pdf-fax";
+      runtimeInputs = [pkgs.ghostscript_headless];
+      text = ''
+        rotation=$((1 + RANDOM % 1000))
+        bool=$((1 + RANDOM % 2))
+        if [ $bool -eq 1 ]; then
+          sign="-"
+        else
+          sign="+"
+        fi
+        ${pkgs.imagemagick}/bin/magick -density 150 "$1" -rotate "''${sign}0.$rotation" -attenuate 0.4 +noise Multiplicative -attenuate 0.03 +noise Multiplicative -sharpen 0x1.0 -colorspace Gray "$2"
+      '';
+    };
+  in
+    lib.mkIf cfg.enable {
+      home.packages = with pkgs; [
+        curl
+        eza # ls replacement
+        fax-pdf
+        file
+        httpie
+        jq
+        mob # smooth git handover
+        tokei # count lines of code
+        tldr # better man pages
+        unzip
+        wget
+        zip
+      ];
+      programs.fzf.enable = true; # fuzzy finding
+      programs.ripgrep.enable = true; # recursive grep
+      programs.zoxide.enable = true; # smart cd replacement
+    };
 }
