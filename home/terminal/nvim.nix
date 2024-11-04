cb4ab1aa4e44f7b41bf864dda07788a447d80613
Benedikt Ritter <beneritter@gmail.com>
2024-11-04 14:00:10 +0100

    home (nvim): More tweaking
    Most notably around tab behavior

--- home/terminal/nvim.nix
+++ home/terminal/nvim.nix
@@ -29,7 +29,29 @@ in {
         number = true;
         relativenumber = true;
 
+        # Set tabs to 2 spaces
+        tabstop = 2;
+        softtabstop = 2;
+        showtabline = 2;
+        expandtab = true;
+
+        # Enable auto indenting and set it to spaces
+        smartindent = true;
+        shiftwidth = 2;
+
+        # Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+        breakindent = true;
+
+        # Enable 24-bit colors
+        termguicolors = true;
+
+        # Enable the sign column to prevent the screen from jumping
+        signcolumn = "yes";
+
+        # Highlight cursor line
         cursorline = true;
+
+        # Remaining lines visibile before starting to scroll
         scrolloff = 5;
 
         swapfile = false;
