911d131e3f03d594560da343cae1e4d3b80142d0
Benedikt Ritter <beneritter@gmail.com>
2024-11-18 11:31:18 +0100

    home (nvim): Extract completion config into dedicated file

---
+++ home/terminal/nvim/completion.nix
@@ -0,0 +1,38 @@
+_: {
+  programs.nixvim.plugins = {
+    # Review the configuration for cmp after switching to 24.11
+    cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        };
+        snippet = {
+          expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+        };
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "luasnip";}
+          {name = "buffer";}
+        ];
+      };
+    };
+    cmp_luasnip.enable = true;
+    luasnip = {
+      enable = true;
+      extraConfig = {
+        enable_autosnippets = true;
+        store_selection_keys = "<Tab>";
+      };
+      fromVscode = [{}];
+    };
+    friendly-snippets.enable = true;
+  };
+}
