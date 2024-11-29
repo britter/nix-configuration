britter/nix-configuration

commit 4fb7d7407cbde5d3543e025cf911fd84247efdfa

hosts (srv-test-2): Add NextCloud synx script (#78)
The script uses ssh, scp, and rsync to connect to and transfer data from
srv-prod-2. Data being present on srv-test-2 NextCloud is dropped in the
process.

systems/x86_64-linux/srv-prod-2/configuration.nix
@@ -22,6 +22,8 @@
     };
   };
 
+  users.users.nextcloud.openssh.authorizedKeys.keyFiles = [../srv-test-2/ssh_srv-test-2_ed25519_key.pub];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave

systems/x86_64-linux/srv-test-2/configuration.nix
@@ -1,6 +1,7 @@
 {...}: {
   imports = [
     ../../../modules
+    ./nextcloud-sync.nix
   ];
 
   my = {

systems/x86_64-linux/srv-test-2/nextcloud-sync.nix
@@ -1,0 +1,29 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  nextcloud-sync = let
+    prodHost = config.my.homelab.srv-prod-2.ip;
+    nextcloudUser = "nextcloud";
+    dataDir = "/var/lib/nextcloud/data";
+    dbName = "nextcloud";
+    dbDump = "/tmp/nextcloud-prod-dump.sql";
+  in
+    pkgs.writeShellApplication {
+      name = "nextcloud-sync";
+      runtimeInputs = [pkgs.rsync];
+      text = ''
+        ssh ${nextcloudUser}@${prodHost} "pg_dump --username=${nextcloudUser} --file=${dbDump} ${dbName}"
+        scp ${nextcloudUser}@${prodHost}:${dbDump} ${dbDump}
+        ssh ${nextcloudUser}@${prodHost} "rm -f ${dbDump}"
+        rsync -avz --delete ${nextcloudUser}@${prodHost}:${dataDir} ${dataDir}
+        sudo -u ${nextcloudUser} psql --command="DROP DATABASE IF EXISTS ${dbName};"
+        sudo -u ${nextcloudUser} psql --command="CREATE DATABASE ${dbName} OWNER ${nextcloudUser};"
+        sudo -u ${nextcloudUser} psql --dbname=${dbName} --file=${dbDump}
+        rm -f ${dbDump}
+      '';
+    };
+in {
+  environment.systemPackages = [nextcloud-sync];
+}

systems/x86_64-linux/srv-test-2/ssh_srv-test-2_ed25519_key.pub
@@ -1,0 +1,1 @@
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE49YvpRLFot3FIVI16tmHEOxvO2LCKP2+40C08QT97d bene@pulse-14

commit f6936df4fb544edadb5f26f15a8101ac2fa3c369

hosts (srv-prod-2): Configure a shell for user nextcloud
Otherwise interactive and non-interactive SSH sessions do not work which
breaks the nextcloud-sync script introduced in
4fb7d7407cbde5d3543e025cf911fd84247efdfa.

systems/x86_64-linux/srv-prod-2/configuration.nix
@@ -22,7 +22,10 @@
     };
   };
 
-  users.users.nextcloud.openssh.authorizedKeys.keyFiles = [../srv-test-2/ssh_srv-test-2_ed25519_key.pub];
+  users.users.nextcloud = {
+    openssh.authorizedKeys.keyFiles = [../srv-test-2/ssh_srv-test-2_ed25519_key.pub];
+    useDefaultShell = true;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions