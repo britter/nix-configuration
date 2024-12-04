39923182b8227a8e2934de6fb2c155525bc570c4
Benedikt Ritter <beneritter@gmail.com>
2024-12-04 12:20:05 +0100

    hosts (srv-test-2): Fix nextcloud-sync script
    The script was trying to connect via srv-prod-2's IP address but the ssh
    config uses 'srv-prod-2' as host identifier. After this change, the
    script uses the host identifier from the ssh configuration and also does
    no longer pass the user name to connect with, because that's also
    configured in ssh config.

--- systems/x86_64-linux/srv-test-2/nextcloud-sync.nix
+++ systems/x86_64-linux/srv-test-2/nextcloud-sync.nix
@@ -3,8 +3,8 @@
   pkgs,
   ...
 }: let
+  prodHost = "srv-prod-2";
   nextcloud-sync = let
-    prodHost = config.my.homelab.srv-prod-2.ip;
     nextcloudUser = "nextcloud";
     dataDir = "/var/lib/nextcloud/data";
     dbName = "nextcloud";
@@ -15,10 +15,10 @@
       runtimeInputs = [pkgs.rsync config.services.nextcloud.occ];
       text = ''
         nextcloud-occ maintenance:mode --on
-        ssh ${nextcloudUser}@${prodHost} "pg_dump --username=${nextcloudUser} --file=${dbDump} ${dbName}"
-        scp ${nextcloudUser}@${prodHost}:${dbDump} ${dbDump}
-        ssh ${nextcloudUser}@${prodHost} "rm -f ${dbDump}"
-        rsync -avz --delete ${nextcloudUser}@${prodHost}:${dataDir} ${dataDir}
+        ssh ${prodHost} "pg_dump --username=${nextcloudUser} --file=${dbDump} ${dbName}"
+        scp ${prodHost}:${dbDump} ${dbDump}
+        ssh ${prodHost} "rm -f ${dbDump}"
+        rsync -avz --delete ${prodHost}:${dataDir} ${dataDir}
         sudo -u postgres psql --command="DROP DATABASE IF EXISTS ${dbName};"
         sudo -u postgres psql --command="CREATE DATABASE ${dbName} OWNER ${nextcloudUser};"
         sudo -u ${nextcloudUser} psql --dbname=${dbName} --file=${dbDump}
@@ -28,4 +28,12 @@
     };
 in {
   environment.systemPackages = [nextcloud-sync];
+
+  programs.ssh.extraConfig = ''
+    Host ${prodHost}
+      HostName ${config.my.homelab.srv-prod-2.ip}
+      User nextcloud
+      IdentityFile /etc/ssh/ssh_host_ed25519_key
+      IdentitiesOnly yes
+  '';
 }
