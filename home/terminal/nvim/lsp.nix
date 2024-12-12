08b1e455ed7f42a9772717f1a592f6e5377db40e
Benedikt Ritter <beneritter@gmail.com>
2024-12-12 20:45:15 +0100

    home (nvim): Automatically format code on buffer write

--- home/terminal/nvim/lsp.nix
+++ home/terminal/nvim/lsp.nix
@@ -7,13 +7,25 @@ _: {
       nixd.enable = true;
       ts_ls.enable = true;
     };
+    onAttach =
+      # lua
+      ''
+        if client.supports_method('textDocument/formatting') then
+          -- Format the current buffer on save
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = buffer;
+            callback = function()
+              vim.lsp.buf.format({ bufnr = buffer, id = client.id })
+            end,
+          })
+        end
+      '';
     keymaps = {
       lspBuf = {
         K = "hover";
         gd = "definition";
         "<leader>ca" = "code_action";
         "<leader>rr" = "rename";
-        "<leader>gq" = "format";
       };
       diagnostic = {
         "[d" = "goto_prev";
