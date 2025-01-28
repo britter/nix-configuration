12403b874d1f0742f58e23318b133eac3f9fb417
Benedikt Ritter <beneritter@gmail.com>
2025-01-28 10:04:49 +0100

    home (desktop): Add Audacity

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -22,6 +22,7 @@ in {
   config = lib.mkIf cfg.enable {
     # software not available as Home Manager module
     home.packages = with pkgs; [
+      audacity
       bitwarden-desktop
       libreoffice
       obsidian
