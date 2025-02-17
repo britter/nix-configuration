520027d9f5e4ae3deec9b183dc68e4d82590e645
Benedikt Ritter <beneritter@gmail.com>
2025-02-17 19:24:12 +0100

    hosts (srv-eval-1): Enable calibre-web

--- systems/x86_64-linux/srv-eval-1/configuration.nix
+++ systems/x86_64-linux/srv-eval-1/configuration.nix
@@ -17,6 +17,12 @@
     };
   };
 
+  services.calibre-web = {
+    enable = true;
+    openFirewall = true;
+    options.enableBookUploading = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
