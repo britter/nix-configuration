1e728d25c8f24e53189072fe23ba755bc6b95240
Benedikt Ritter <beneritter@gmail.com>
2025-02-18 09:41:17 +0100

    home (sway): Configure Bitwarden unlock window to be floating

--- home/desktop/sway/sway.nix
+++ home/desktop/sway/sway.nix
@@ -192,6 +192,10 @@ in {
             criteria.app_id = "qalculate-gtk";
             command = "resize set 800 600";
           }
+          {
+            criteria.title = "Extension: (Bitwarden Password Manager) - Bitwarden — Mozilla Firefox";
+            command = "floating enable";
+          }
         ];
       };
     };
