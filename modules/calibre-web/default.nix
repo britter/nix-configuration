a8aebec116115d6c298f03ac505b22e8aa6261b4
Benedikt Ritter <beneritter@gmail.com>
2025-03-04 16:49:59 +0100

    modules (calibre-web): Configure path to library

--- modules/calibre-web/default.nix
+++ modules/calibre-web/default.nix
@@ -9,28 +9,36 @@ in {
     enable = lib.mkEnableOption "calibre-web";
   };
 
-  config = lib.mkIf cfg.enable {
-    services.calibre-web = {
-      enable = true;
-      listen.ip = "0.0.0.0";
-      options = {
-        enableBookConversion = true;
-        enableBookUploading = true;
-        enableKepubify = true;
+  config = let
+    calibreLibraryPath = "/var/lib/calibre-library";
+  in
+    lib.mkIf cfg.enable {
+      services.calibre-web = {
+        enable = true;
+        listen.ip = "0.0.0.0";
+        options = {
+          enableBookConversion = true;
+          enableBookUploading = true;
+          enableKepubify = true;
+          calibreLibrary = calibreLibraryPath;
+        };
+      };
+
+      systemd.tmpfiles.rules = [
+        "d ${calibreLibraryPath} 0755 calibre-web calibre-web"
+      ];
+
+      my.modules.https-proxy = {
+        configurations = [
+          {
+            fqdn = "books.${config.my.host.name}.ritter.family";
+            aliases = ["books.ritter.family"];
+            target = "http://localhost:8083";
+            extraConfig = ''
+              client_max_body_size 100M;
+            '';
+          }
+        ];
       };
     };
-
-    my.modules.https-proxy = {
-      configurations = [
-        {
-          fqdn = "books.${config.my.host.name}.ritter.family";
-          aliases = ["books.ritter.family"];
-          target = "http://localhost:8083";
-          extraConfig = ''
-            client_max_body_size 100M;
-          '';
-        }
-      ];
-    };
-  };
 }
