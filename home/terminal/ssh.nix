5e97771b777ff3b3b3354631801eb0efd3e4644f
Benedikt Ritter <beneritter@gmail.com>
2025-03-28 15:19:00 +0100

    home (ssh): Tweaks
    - Use DNS to resolve hosts
    - Add workaround for ghostty over SSH

--- home/terminal/ssh.nix
+++ home/terminal/ssh.nix
@@ -21,35 +21,55 @@ in {
           identityFile = privateKey;
           identitiesOnly = true;
         };
-        directions = {
-          hostname = osConfig.my.homelab.directions.ip;
+        "directions.ritter.family" = {
           identityFile = privateKey;
           user = "root";
+          # fix for ghostty until servers update to ncurses 6.5+, see https://ghostty.org/docs/help/terminfo
+          setEnv = {
+            TERM = "xterm-256color";
+          };
         };
-        "srv-prod-1" = {
-          hostname = osConfig.my.homelab.srv-prod-1.ip;
+        "srv-prod-1.ritter.family" = {
           identityFile = privateKey;
           user = "root";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
         };
-        "srv-prod-2" = {
-          hostname = osConfig.my.homelab.srv-prod-2.ip;
+        "srv-prod-2.ritter.family" = {
           identityFile = privateKey;
           user = "root";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
         };
-        "srv-test-1" = {
-          hostname = osConfig.my.homelab.srv-test-1.ip;
+        "srv-test-1.ritter.family" = {
           identityFile = privateKey;
           user = "root";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
         };
-        "srv-test-2" = {
-          hostname = osConfig.my.homelab.srv-test-2.ip;
+        "srv-test-2.ritter.family" = {
           identityFile = privateKey;
           user = "root";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
         };
-        "srv-eval-1" = {
-          hostname = osConfig.my.homelab.srv-eval-1.ip;
+        "srv-eval-1.ritter.family" = {
           identityFile = privateKey;
           user = "root";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
+        };
+        "srv-backup-1.ritter.family" = {
+          identityFile = privateKey;
+          user = "root";
+          setEnv = {
+            TERM = "xterm-256color";
+          };
         };
         "git.ritter.family" = {
           hostname = osConfig.my.homelab.srv-prod-2.ip;
