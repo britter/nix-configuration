d3d827a2a31ab7222a77f11408710ae78e56642a
Benedikt Ritter <beneritter@gmail.com>
2025-04-28 11:24:26 +0200

    home (waybar): Tweak default output and calendar

--- home/desktop/sway/waybar.nix
+++ home/desktop/sway/waybar.nix
@@ -40,8 +40,22 @@ in
           ];
 
           "clock" = {
-            tooltip-format = "<big>{:%B %Y}</big>\n<tt><small>{calendar}</small></tt>";
-            format-alt = "{:%d.%m.%y}";
+            # See https://fmt.dev/latest/syntax/#chrono-format-specifications
+            format = "{:%a, %b %d - %R}";
+            tooltip-format = "<tt><small>{calendar}</small></tt>";
+            calendar = {
+              mode = "year";
+              mode-mon-col = 3;
+              weeks-pos = "right";
+              on-scroll = 1;
+              format = {
+                months = "<span color='#ffead3'><b>{}</b></span>";
+                days = "<span color='#ecc6d9'><b>{}</b></span>";
+                weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+                weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+                today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+              };
+            };
           };
 
           "cpu" = {
