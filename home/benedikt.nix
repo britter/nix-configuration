634d5a63b2dbe97ac8e45df7502f694a3f7c6d9c
Benedikt Ritter <benedikt.ritter@chainguard.dev>
2025-05-05 11:55:32 +0200

    home (standalone): Set up gitsign with systemd unit activated cache

--- home/benedikt.nix
+++ home/benedikt.nix
@@ -1,4 +1,8 @@
-{ config, ... }:
+{
+  pkgs,
+  config,
+  ...
+}:
 
 {
   imports = [
@@ -26,7 +30,6 @@
       enable = true;
       git.addIncludes = false;
       ssh.enable = false;
-      gpg.enable = false;
     };
   };
   programs.ssh = {
@@ -35,6 +38,52 @@
       "github.com" = {
         identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
         identitiesOnly = true;
+      };
+    };
+  };
+  programs.git = {
+    includes = [
+      {
+        condition = "gitdir:~/github/chainguard-dev/";
+        contents = {
+          gpg.x509.program = "${pkgs.gitsign}/bin/gitsign";
+          gpg.format = "x509";
+          gitsign.connectorID = "https://accounts.google.com";
+        };
+      }
+    ];
+  };
+  home.sessionVariables = {
+    GITSIGN_CREDENTIAL_CACHE = "${config.home.homeDirectory}/.cache/sigstore/gitsign/cache.sock";
+  };
+  systemd.user = {
+    enable = true;
+    services = {
+      gitsign-credential-cache = {
+        Unit = {
+          Description = "GitSign credential cache";
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.gitsign}/bin/gitsign-credential-cache";
+        };
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
+      };
+    };
+    sockets = {
+      gitsign-credential-cache = {
+        Unit = {
+          Description = "GitSign credential cache socket";
+        };
+        Socket = {
+          ListenStream = "%C/sigstore/gitsign/cache.sock";
+          DirectoryMode = "0700";
+        };
+        Install = {
+          WantedBy = [ "default.target" ];
+        };
       };
     };
   };
