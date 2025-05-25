1fc8a6ce6360f1bdddb6bfc89cbcb7e56115fc58
Benedikt Ritter <beneritter@gmail.com>
2025-05-25 21:06:00 +0200

    home (tmux): Update config to new catppuccin tmux plugin

--- home/terminal/tmux.nix
+++ home/terminal/tmux.nix
@@ -35,25 +35,14 @@ in
           extraConfig = ''
             set-option -g status-position top
 
-            set -g @catppuccin_window_left_separator ""
-            set -g @catppuccin_window_right_separator " "
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
+            set -g @catppuccin_window_status_style "rounded"
 
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
-
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W"
-
-            set -g @catppuccin_status_modules_right "directory session"
-            set -g @catppuccin_status_left_separator  " "
-            set -g @catppuccin_status_right_separator ""
-            set -g @catppuccin_status_right_separator_inverse "no"
-            set -g @catppuccin_status_fill "icon"
-            set -g @catppuccin_status_connect_separator "no"
-
-            set -g @catppuccin_directory_text "#{pane_current_path}"
+            set -g status-right-length 100
+            set -g status-left-length 100
+            set -g status-left ""
+            set -g status-right "#{E:@catppuccin_status_application}"
+            set -ag status-right "#{E:@catppuccin_status_session}"
+            set -ag status-right "#{E:@catppuccin_status_uptime}"
           '';
         }
       ];
