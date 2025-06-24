054d2f8e7b903c25898d23d4216e1b1bc83163b3
Benedikt Ritter <beneritter@gmail.com>
2025-06-24 21:31:52 +0200

    hosts (framework-13): Init

---
+++ systems/x86_64-linux/framework-13/configuration.nix
@@ -0,0 +1,52 @@
+{
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    #    inputs.nixos-facter-modules.nixosModules.facter
+    ../../../modules
+  ];
+
+  # facter.reportPath = ./facter.json;
+  # see https://github.com/numtide/nixos-facter-modules/issues/62
+  # facter.detected.dhcp.enable = false;
+
+  my = {
+    host = {
+      role = "desktop";
+    };
+    user.signingKey = "394546A47BB40E12";
+    modules = {
+      disko = {
+        enable = true;
+        bootDisk = "/dev/nvme0n1";
+        swapSize = "64GB";
+      };
+    };
+  };
+
+  services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    # Enables mDNS NSS plugin for IPv4. Enabling it allows applications to resolve names in the .local domain
+    # This is required for printing on a HP network printer
+    nssmdns4 = true;
+  };
+
+  services.tailscale.enable = true;
+
+  services.gnome.gnome-keyring.enable = true;
+  security.polkit.enable = true;
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
+  };
+  environment.systemPackages = [
+    pkgs.podman-compose
+  ];
+  programs.nix-ld.enable = true;
+
+  system.stateVersion = "25.05";
+}
