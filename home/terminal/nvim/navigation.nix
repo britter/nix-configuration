9fa3642343a4dd9f16fdde946e479d23d351885e
Benedikt Ritter <beneritter@gmail.com>
2025-06-27 10:18:23 +0200

    home (nvim): Center view when moving horizontally

--- home/terminal/nvim/navigation.nix
+++ home/terminal/nvim/navigation.nix
@@ -2,6 +2,7 @@ _: {
   programs.nixvim = {
     plugins.tmux-navigator.enable = true;
     keymaps = [
+      # Jump through and delete buffers
       {
         action = "<cmd>bprev<CR>";
         key = "[b";
@@ -17,6 +18,7 @@ _: {
         key = "<leader>bd";
         mode = [ "n" ];
       }
+      # Jump through quickfix list
       {
         action = "<cmd>cprevious<CR>";
         key = "[q";
@@ -25,6 +27,28 @@ _: {
       {
         action = "<cmd>cnext<CR>";
         key = "]q";
+        mode = [ "n" ];
+      }
+      # Remap Crtl-d and Crtl-z to also center the view
+      {
+        action = "<C-d>zz";
+        key = "<C-d>";
+        mode = [ "n" ];
+      }
+      {
+        action = "<C-u>zz";
+        key = "<C-u>";
+        mode = [ "n" ];
+      }
+      # Remap jumping trough search results to also center the view
+      {
+        action = "nzz";
+        key = "n";
+        mode = [ "n" ];
+      }
+      {
+        action = "Nzz";
+        key = "N";
         mode = [ "n" ];
       }
     ];
