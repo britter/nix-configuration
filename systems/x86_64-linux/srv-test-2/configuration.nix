d37c5b579622e7d2749aed808e7855d70acefc1c
Benedikt Ritter <beneritter@gmail.com>
2025-10-11 11:11:43 +0200

    hosts (srv-test-2): Fix git restore service
    - align naming with backup services
    - align configuration with backup service, including cache dir, unit
      dependencies, etc.
    - Configure restore to root directory with an include for all files
      included in the snapshot.
    
    The last item is particularly important. We want the restore to put the
    files into their real location, hence we need to restore to root. We
    can't restore to a different location because for large backups, such as
    nextcloud it would require excessive file copying. We also can't use
    `lastest:/path/to/file --target /path/to/file` because for vaultwarden
    and nextcloud we have multiple paths in the backup. Running multiple
    restore commands, one for each path would we an option, but there's the
    danger of a snapshot being created in the middle resulting in files
    being restored from different snapshots. This is because we don't lock
    the repository because the backup user is read only.
    The solution is to restore to the root directory, but due to the use of
    `--delete` we also need to specify the paths we want to restore.
    Otherwise restic fails with an error.

--- systems/x86_64-linux/srv-test-2/configuration.nix
+++ systems/x86_64-linux/srv-test-2/configuration.nix
@@ -56,7 +56,6 @@
       AWS_ACCESS_KEY_ID=${config.sops.placeholder."restic/git/minio-access-key-id"}
       AWS_SECRET_ACCESS_KEY=${config.sops.placeholder."restic/git/minio-secret-access-key"}
       RESTIC_PASSWORD=${config.sops.placeholder."restic/git/repository-password"}
-      RESTIC_REPOSITORY=s3:https://minio.srv-prod-3.ritter.family/restic-backups/git;
     '';
   };
   sops.secrets."restic/nextcloud/repository-password" = { };
@@ -88,7 +87,7 @@
     repository = "s3:https://minio.srv-prod-3.ritter.family/restic-backups/git";
     timerConfig = null;
   };
-  systemd.timers.restic-restore-git = {
+  systemd.timers.restic-restores-git = {
     description = "Timer for restoring git";
     wantedBy = [ "timers.target" ];
     timerConfig = {
@@ -96,12 +95,23 @@
       Persistent = true;
     };
   };
-  systemd.services.restic-restore-git = {
+  systemd.services.restic-restores-git = {
     description = "Runs restic restore to restore the git repository";
+    environment = {
+      RESTIC_CACHE_DIR = "%C/restic-restores-git";
+      RESTIC_REPOSITORY = "s3:https://minio.srv-prod-3.ritter.family/restic-backups/git";
+    };
+    after = [ "network.online.target" ];
+    wants = [ "network.online.target" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${lib.getExe pkgs.restic} restore latest:/srv/git --delete --no-lock --target /srv/git";
+      ExecStart = "${lib.getExe pkgs.restic} restore latest:/srv/git --no-lock --include /srv/git --delete --target /";
       EnvironmentFile = config.sops.templates."restic/git/secrets.env".path;
+      RestartIfChanged = false;
+      CacheDirectory = "restic-restores-git";
+      CacheDirectoryMode = "0700";
+      PrivateTmp = true;
+      RuntimeDirectory = "restic-restores-git";
     };
   };
 
