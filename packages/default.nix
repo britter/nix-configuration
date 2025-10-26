d3b3c01f11e74fd6d7075e47c5dc14855df205c8
Benedikt Ritter <beneritter@gmail.com>
2025-10-26 16:04:30 +0100

    fritzbox-cloudflare-dyndns: remove package

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 with pkgs;
 {
-  fritzbox-cloudflare-dyndns = callPackage ./fritzbox-cloudflare-dyndns { };
   gh-get = callPackage ./gh-get { };
   groovy-language-server = callPackage ./groovy-language-server { };
 }
