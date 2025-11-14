df89a00af279b852f0df66a9c78bc76c4e955954
Benedikt Ritter <beneritter@gmail.com>
2025-11-14 10:34:58 +0100

    terminal (tools): Add captive-portal utility
    Will open the default gateway's IP address in a browser. Most
    public WiFi gateways redirect that to their captive portal.
    Unforutnately this isn't the case for the WIFIonICE network.
    Hence it's handled explicitly.

--- home/terminal/tools.nix
+++ home/terminal/tools.nix
@@ -27,10 +27,23 @@ in
           ${pkgs.imagemagick}/bin/magick -density 150 "$1" -rotate "''${sign}0.$rotation" -attenuate 0.4 +noise Multiplicative -attenuate 0.03 +noise Multiplicative -sharpen 0x1.0 -colorspace Gray "$2"
         '';
       };
+      captive-portal = pkgs.writeShellApplication {
+        name = "captive-portal";
+        runtimeInputs = [ pkgs.networkmanager ];
+        text = ''
+          PORTAL_URL=http://"$(ip -oneline route get 1.1.1.1 | awk '{print $3}')"
+          if nmcli -t -f name connection show --active | grep -qx "WIFIonICE"; then
+              PORTAL_URL="https://login.wifionice.de"
+          fi
+
+          xdg-open "$PORTAL_URL"
+        '';
+      };
     in
     lib.mkIf cfg.enable {
       home.packages = with pkgs; [
         caligula # tui for disk imaging
+        captive-portal
         curl
         eza # ls replacement
         fd # find replacement
