75aa90f87fc759535f0cba46996c6dc871be634a
Benedikt Ritter <beneritter@gmail.com>
2025-11-20 11:55:45 +0100

    home (nvim): Configure alloy fmt as none-ls formatting source

--- home/terminal/nvim/languages/alloy.nix
+++ home/terminal/nvim/languages/alloy.nix
@@ -24,23 +24,45 @@ in
     filetype.extension = {
       alloy = "alloy";
     };
-    plugins.treesitter = {
-      grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars ++ [
-        treesitter-alloy-grammar
-      ];
-      luaConfig.post =
-        # lua
-        ''
-          do
-            local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-            parser_config.alloy = {
+    plugins = {
+      treesitter = {
+        grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars ++ [
+          treesitter-alloy-grammar
+        ];
+        luaConfig.post =
+          # lua
+          ''
+            do
+              local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+              parser_config.alloy = {
               install_info = {
-                url = "${treesitter-alloy-grammar}",
-                files = {"src/parser.c"},
-              },
-            }
-          end
-        '';
+              url = "${treesitter-alloy-grammar}",
+                  files = {"src/parser.c"},
+                },
+              }
+            end
+          '';
+      };
+      none-ls = {
+        luaConfig.post =
+          # lua
+          ''
+            do
+              local null_ls = require("null-ls")
+              local alloy_fmt = {
+                method = null_ls.methods.FORMATTING,
+                filetypes = { "alloy" },
+                name = "alloy-fmt",
+                generator = require("null-ls.helpers").formatter_factory({
+                  command = "${pkgs.grafana-alloy}/bin/alloy",
+                  args = { "fmt" },
+                  to_stdin = true,
+                }),
+              }
+              null_ls.register(alloy_fmt)
+            end
+          '';
+      };
     };
 
     extraPlugins = [ treesitter-alloy-grammar ];
