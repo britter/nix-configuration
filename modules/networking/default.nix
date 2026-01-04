3a72377a179c6d758a83b382f400afb26186f849
Benedikt Ritter <beneritter@gmail.com>
2026-01-04 14:09:42 +0100

    modules (networking): use DHCP to assign IP addresses
    Initially I used static IP address assignment because it was simplest.
    Now, I realize that changing the network topology becomes crumbersome
    due to the fact that machines may end up in the wrong subnet.
    For that reason I realized that defining IPs centrally using DHCP
    is easier, although I have to remove IP config from the nix configuration
    and store it in my UDM Pro instead.

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -1,17 +1,11 @@
 {
   config,
   lib,
-  home-lab,
   ...
 }:
 let
   cfg = config.my.modules.networking;
   hostCfg = config.my.host;
-  homelabCfg =
-    if (builtins.hasAttr "${hostCfg.name}" home-lab.hosts) then
-      home-lab.hosts.${hostCfg.name}
-    else
-      null;
 in
 {
   options.my.modules.networking = {
@@ -22,17 +16,6 @@ in
     networking = {
       hostName = hostCfg.name;
       networkmanager.enable = hostCfg.role == "desktop";
-    }
-    // (lib.optionalAttrs (homelabCfg != null && (builtins.hasAttr "ip" homelabCfg)) {
-      usePredictableInterfaceNames = false;
-      interfaces.eth0.ipv4.addresses = [
-        {
-          address = homelabCfg.ip;
-          prefixLength = 24;
-        }
-      ];
-      defaultGateway = home-lab.devices.fritz-box.ip;
-      nameservers = [ home-lab.hosts.directions.ip ];
-    });
+    };
   };
 }
