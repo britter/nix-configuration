d34fb59059fe0f95894c239b7d1f3cca4828e8dc
Benedikt Ritter <beneritter@gmail.com>
2026-01-06 10:21:41 +0100

    overlays: apply fix for weasyprint
    This package is broken in nixos-unstable due to a failing test. Since
    weasyprint is a dependency of stirling-pdf, srv-prod-2 configuration
    currently fails to build.
    This change adds an overlay that applys the changes from
    https://github.com/NixOS/nixpkgs/pull/477155 to make weasyprint build
    again.

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,4 +2,17 @@
   my-pkgs,
   nur,
 }:
-_final: _prev: my-pkgs // (nur.overlays.default _final _prev)
+_final: prev:
+my-pkgs
+// (nur.overlays.default _final prev)
+// {
+  # https://github.com/NixOS/nixpkgs/pull/477155
+  python3 = prev.python3.override {
+    packageOverrides = _pyfinal: pyprev: {
+      weasyprint = pyprev.weasyprint.overrideAttrs (attrs: {
+        disabledTests = attrs.disabledTests ++ [ "test_2d_transform" ];
+      });
+    };
+  };
+
+}
