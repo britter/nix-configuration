d2d35a79cf9987287c3868e1785cd47d6ffad00a
Benedikt Ritter <beneritter@gmail.com>
2026-01-12 11:37:57 +0100

    flake: fix deprecation in pre-commit-hooks config

--- flake.nix
+++ flake.nix
@@ -56,7 +56,7 @@
             src = ./.;
             hooks = {
               deadnix.enable = true;
-              nixfmt-rfc-style.enable = true;
+              nixfmt.enable = true;
             };
           };
           formatting = treefmtEval.config.build.check self;
