3cb7c90d5d607350e4ab3ab11cc3691bf182d26f
Benedikt Ritter <beneritter@gmail.com>
2026-01-22 20:56:43 +0100

    Revert "overlays: apply fix for weasyprint"
    This reverts commit d34fb59059fe0f95894c239b7d1f3cca4828e8dc.

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,17 +2,4 @@
   my-pkgs,
   nur,
 }:
-_final: prev:
-my-pkgs
-// (nur.overlays.default _final prev)
-// {
-  # https://github.com/NixOS/nixpkgs/pull/477155
-  python3 = prev.python3.override {
-    packageOverrides = _pyfinal: pyprev: {
-      weasyprint = pyprev.weasyprint.overrideAttrs (attrs: {
-        disabledTests = attrs.disabledTests ++ [ "test_2d_transform" ];
-      });
-    };
-  };
-
-}
+_final: _prev: my-pkgs // (nur.overlays.default _final _prev)
