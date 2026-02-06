28b89501070d2ba17010250df0b64ecd97486ed1
Benedikt Ritter <beneritter@gmail.com>
2026-02-06 21:18:38 +0100

    hosts (srv-prod-3-new): init host

--- systems/x86_64-linux/srv-prod-3-new/configuration.nix
+++ systems/x86_64-linux/srv-prod-3-new/configuration.nix
@@ -1,9 +1,13 @@
 {
+  config,
+  lib,
+  pkgs,
   ...
 }:
 {
   imports = [
     ../../../modules
+    ./disko-config.nix
   ];
 
   my = {
@@ -12,15 +16,59 @@
     };
     modules = {
       proxmox-vm.enable = true;
-      disko = {
-        enable = true;
-        bootDisk = "/dev/sda";
-      };
       minio.enable = true;
-      tailscale.enable = true;
     };
   };
 
+  sops.secrets."srv-prod-3/minio/access-key" = { };
+  sops.secrets."srv-prod-3/minio/secret-key" = { };
+  sops.templates."rclone.conf" = {
+    content = ''
+      [srv-prod-3]
+      type = s3
+      provider = Minio
+      env_auth = false
+      region = eu-central-1
+      access_key_id = ${config.sops.placeholder."srv-prod-3/minio/access-key"}
+      secret_access_key = ${config.sops.placeholder."srv-prod-3/minio/secret-key"}
+      endpoint = https://minio.srv-prod-3.ritter.family
+      location_constraint =
+      server_side_encryption =
+
+      [srv-prod-3-new]
+      type = s3
+      provider = Minio
+      env_auth = false
+      region = eu-central-1
+      access_key_id = ${config.sops.placeholder."minio/root-user"}
+      secret_access_key = ${config.sops.placeholder."minio/root-password"}
+      endpoint = http://localhost:9000
+      location_constraint =
+      server_side_encryption =
+    '';
+  };
+
+  # nightly minio-sync
+  systemd.timers.nightly-minio-sync = {
+    description = "Nightly timer to wake up the system for the minio sync";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "02:00";
+      Persistent = true;
+      WakeSystem = true;
+    };
+  };
+  systemd.services.nightly-minio-sync = {
+    description = "Nightly minio sync that suspends the system after running";
+    serviceConfig = {
+      Type = "oneshot";
+      # prefix ExecStart with - so that ExecStartPost is executed even if sync fails
+      ExecStart = "-${lib.getExe pkgs.rclone} sync srv-prod-3: srv-prod-3-new: --config ${
+        config.sops.templates."rclone.conf".path
+      }";
+      ExecStartPost = "${pkgs.systemd}/bin/systemctl suspend";
+    };
+  };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
---
+++ systems/x86_64-linux/srv-prod-3-new/disko-config.nix
@@ -0,0 +1,43 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.disko.nixosModules.disko
+  ];
+  disko.devices = {
+    disk.main = {
+      device = "/dev/sda";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "defaults" ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [ "noatime" ];
+            };
+          };
+        };
+      };
+    };
+  };
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      configurationLimit = 20;
+    };
+    efi.canTouchEfiVariables = true;
+  };
+}
