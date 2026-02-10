714f612f4a7f375ace6377d832276aeaf212668e
Benedikt Ritter <beneritter@gmail.com>
2026-02-10 11:39:05 +0100

    home (nvim): disable zls due to build failures
    Should be enables again once the fix lands in nixos-unstable.

--- home/terminal/nvim/lsp.nix
+++ home/terminal/nvim/lsp.nix
@@ -14,7 +14,8 @@
         };
         nixd.enable = true;
         ts_ls.enable = true;
-        zls.enable = true;
+        # disabled due to build failures in nixpkgs, see https://github.com/NixOS/nixpkgs/issues/488701
+        zls.enable = false;
       };
       luaConfig.pre = ''
         local augroup = vim.api.nvim_create_augroup('Autoformat', {})
