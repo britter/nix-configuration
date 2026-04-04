aec58a24e3d0bb5373392d964e794d6842fa1c6e
Benedikt Ritter <beneritter@gmail.com>
2026-04-04 21:51:02 +0200

    hosts (srv-prod-2): Mount nfs storage for nextcloud

--- systems/x86_64-linux/srv-prod-2/configuration.nix
+++ systems/x86_64-linux/srv-prod-2/configuration.nix
@@ -157,6 +157,12 @@
       };
     };
 
+  boot.supportedFilesystems = [ "nfs" ];
+  fileSystems."/srv/nextcloud-data" = {
+    fsType = "nfs";
+    device = "storage.ritter.family:/mnt/default-pool/nextcloud";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
