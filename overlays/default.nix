9af68e35bd56f848df498b7efc3f8bed5ebea9ae
Benedikt Ritter <beneritter@gmail.com>
2026-04-27 10:08:13 +0200

    overlays: add upstream fix for calibre-web
    See https://github.com/NixOS/nixpkgs/commit/5a4c086431aaeb4e1937c09498d3f90c3e8bd642

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,4 +2,11 @@
   my-pkgs,
   nur,
 }:
-final: prev: my-pkgs // (nur.overlays.default final prev)
+final: prev:
+my-pkgs
+// (nur.overlays.default final prev)
+// {
+  calibre-web = prev.calibre-web.overrideAttrs (prev: {
+    pythonRelaxDeps = prev.pythonRelaxDeps ++ [ "requests" ];
+  });
+}
