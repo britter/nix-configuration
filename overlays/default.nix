e486c98e9ceb2d543dac04fa60b68404e38ea68d
Benedikt Ritter <beneritter@gmail.com>
2026-04-30 14:46:43 +0200

    overlays: opt-out of gh telemetry collection
    This can be removed once https://github.com/NixOS/nixpkgs/pull/515047
    has been merged.

--- overlays/default.nix
+++ overlays/default.nix
@@ -9,4 +9,11 @@ my-pkgs
   calibre-web = prev.calibre-web.overrideAttrs (prev: {
     pythonRelaxDeps = prev.pythonRelaxDeps ++ [ "requests" ];
   });
+  gh = prev.gh.overrideAttrs (prev: {
+    nativeBuildInputs = prev.nativeBuildInputs ++ [ final.makeWrapper ];
+    installPhase = prev.installPhase + ''
+      wrapProgram $out/bin/gh \
+        --set GH_TELEMETRY false
+    '';
+  });
 }
