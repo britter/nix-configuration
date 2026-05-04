f639cec4eda78f5304286539157d2e7c3ee8a307
Benedikt Ritter <beneritter@gmail.com>
2026-05-04 15:48:26 +0200

    gh-get: 2.3.0 -> 2.4.0

--- packages/gh-get/default.nix
+++ packages/gh-get/default.nix
@@ -4,12 +4,12 @@
 }:
 buildGoModule rec {
   pname = "gh-get";
-  version = "2.3.0";
+  version = "2.4.0";
   src = fetchFromGitHub {
     owner = "britter";
     repo = "gh-get";
     rev = "v${version}";
-    sha256 = "sha256-zJoLahWCgKEJydUJZwdw+ZnWh/ciJJzYsVDi3JaXiyQ=";
+    sha256 = "sha256-3J8vrnp/H4CZkectbrumJobDOS5sponR72J6EueMWJU=";
   };
   vendorHash = "sha256-qzGBwWp9W2wZRuVGBGwIhnUZNaV+Dmzr0WSIFZMr6os=";
 }
