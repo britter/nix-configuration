46e909e81effc61343b89a4d8e1b1890787be7fb
Benedikt Ritter <beneritter@gmail.com>
2026-05-28 09:00:37 +0200

    srv-prod-5: migrate to dendritic host pattern

---
+++ modules/hosts/srv-prod-5/configuration.nix
@@ -0,0 +1,39 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.srv-prod-5 = {
+    imports = [
+      ../../../_needs_migration/modules
+      (inputs.self.factory.sops { secretsFile = ./secrets.yaml; })
+    ];
+
+    my = {
+      host = {
+        role = "server";
+      };
+      modules = {
+        proxmox-vm.enable = true;
+        navidrome.enable = true;
+        tailscale.enable = true;
+      };
+    };
+
+    fileSystems."/srv/navidrome-library" = {
+      fsType = "nfs";
+      device = "storage.ritter.family:/mnt/default-pool/navidrome-library";
+    };
+    systemd.services.navidrome = {
+      unitConfig = {
+        RequiresMountsFor = "/srv/navidrome-library";
+      };
+    };
+    services.navidrome.settings.MusicFolder = "/srv/navidrome-library";
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "24.05"; # Did you read the comment?
+  };
+}
