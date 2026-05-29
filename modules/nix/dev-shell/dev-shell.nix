52d6b4f94dc0d84c7d07941a5f151228c9a2d977
Benedikt Ritter <beneritter@gmail.com>
2026-05-29 21:32:34 +0200

    dev-shell: improve dev-shell using gum

--- modules/nix/dev-shell/dev-shell.nix
+++ modules/nix/dev-shell/dev-shell.nix
@@ -2,17 +2,28 @@
 {
   imports = [ inputs.pre-commit-hooks.flakeModule ];
   perSystem =
-    { config, pkgs, ... }:
+    {
+      lib,
+      config,
+      pkgs,
+      ...
+    }:
     {
       devShells.default =
         let
           setup-host = pkgs.writeShellApplication {
             name = "setup-host";
             runtimeInputs = [
+              pkgs.gum
               pkgs.nixos-anywhere
               pkgs.sops
             ];
             runtimeEnv = {
+              CONFIGURATIONS = lib.pipe inputs.self.nixosConfigurations [
+                lib.attrNames
+                (lib.filter (lib.hasPrefix "srv"))
+                (lib.concatStringsSep " ")
+              ];
               HOST_KEYS_FILE = ./host-keys.yaml;
             };
             text = builtins.readFile ./setup-host.sh;
--- modules/nix/dev-shell/setup-host.sh
+++ modules/nix/dev-shell/setup-host.sh
@@ -1,20 +1,11 @@
 # Source (with modifications): https://github.com/nix-community/nixos-anywhere/blob/46dc28f4f89b747084c7dd6d273b1278142220ce/docs/howtos/secrets.md
 
-# Capture host to configure
-if [ -z "$1" ]; then
-  echo "Please provide the host to setup as an argument!"
-  exit 1
-fi
+read -ra choices <<< "$CONFIGURATIONS"
+CONFIGURATION=$(gum choose --header "Select configuration" "${choices[@]}")
+IP=$(gum input --header "Host to deploy configuration to" --placeholder "IP address")
+gum confirm "Deploy $CONFIGURATION to $IP?"
 
-if [ -z "$2" ]; then
-  echo "Please provide the IP to setup as an argument!"
-  exit 1
-fi
-
-host="$1"
-ip="$2"
-
-echo "Setting up $host via $ip..."
+echo "Setting up $CONFIGURATION on $IP..."
 
 # Create a temporary directory
 temp=$(mktemp -d)
@@ -29,16 +20,16 @@ trap cleanup EXIT
 install -d -m755 "$temp/etc/ssh"
 
 # copy public ket to the temporary directory
-sops -- decrypt "$HOST_KEYS_FILE" --extract "[\"$host\"][\"public-key\"]" --output "$temp/etc/ssh/ssh_host_ed25519_key.pub"
+sops -- decrypt "$HOST_KEYS_FILE" --extract "[\"$CONFIGURATION\"][\"public-key\"]" --output "$temp/etc/ssh/ssh_host_ed25519_key.pub"
 # Set the correct permissions so sshd will accept the key
 chmod 644 "$temp/etc/ssh/ssh_host_ed25519_key.pub"
 
 # copy private ket to the temporary directory
-sops -- decrypt "$HOST_KEYS_FILE" --extract "[\"$host\"][\"private-key\"]" --output "$temp/etc/ssh/ssh_host_ed25519_key"
+sops -- decrypt "$HOST_KEYS_FILE" --extract "[\"$CONFIGURATION\"][\"private-key\"]" --output "$temp/etc/ssh/ssh_host_ed25519_key"
 # Set the correct permissions so sshd will accept the key
 chmod 600 "$temp/etc/ssh/ssh_host_ed25519_key"
 
 # Install NixOS to the host system with our secrets
-nixos-anywhere --extra-files "$temp" --flake ".#$host" "root@$ip"
+nixos-anywhere --extra-files "$temp" --flake ".#$CONFIGURATION" "root@$IP"
 
-echo "Done setting up $host via $ip!"
+echo "Done setting up $CONFIGURATION on $IP!"
