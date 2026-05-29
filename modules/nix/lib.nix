2f68428e4e6f9c6f89029c2bfea15bf97a035566
Benedikt Ritter <beneritter@gmail.com>
2026-05-29 14:02:28 +0200

    lib: remove mkNixosLegacy

--- modules/nix/lib.nix
+++ modules/nix/lib.nix
@@ -2,22 +2,6 @@
 {
   flake.lib = rec {
     home-lab = import ../../home-lab.nix;
-    mkNixosLegacy = system: hostName: {
-      ${hostName} = inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {
-          inherit
-            inputs
-            system
-            hostName
-            home-lab
-            ;
-        };
-        modules = [
-          "${toString inputs.self}/systems/${system}/${hostName}/configuration.nix"
-        ];
-      };
-    };
     mkNixos = system: hostName: {
       ${hostName} = inputs.nixpkgs.lib.nixosSystem {
         inherit system;
