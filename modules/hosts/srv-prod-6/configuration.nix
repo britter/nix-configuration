9ad52d998d636dbd6af1b02653c3e3064b22eaa4
Benedikt Ritter <beneritter@gmail.com>
2026-06-28 10:11:36 +0200

    srv-prod-6: init host

---
+++ modules/hosts/srv-prod-6/configuration.nix
@@ -0,0 +1,14 @@
+{ config, ... }:
+{
+  flake.modules.nixos.srv-prod-6 = {
+    imports = with config.flake.modules.nixos; [
+      system-server
+      proxmox-vm
+      tailscale
+      beszel-agent
+      (config.flake.factory.sops { secretsFile = ./secrets.yaml; })
+    ];
+
+    system.stateVersion = "26.05";
+  };
+}
