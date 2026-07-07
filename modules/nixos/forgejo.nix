e8fdcbe6553c733f7c61b5f8938f4e10795f8093
Benedikt Ritter <beneritter@gmail.com>
2026-07-07 14:25:15 +0200

    forgejo: declarative management of admin user

--- modules/nixos/forgejo.nix
+++ modules/nixos/forgejo.nix
@@ -1,6 +1,6 @@
 _: {
   flake.modules.nixos.forgejo =
-    { config, ... }:
+    { config, lib, ... }:
     {
       services.forgejo = {
         enable = true;
@@ -16,6 +16,19 @@ _: {
           service.DISABLE_REGISTRATION = true;
         };
       };
+
+      sops.secrets."forgejo/admin-user-password".owner = "forgejo";
+      systemd.services.forgejo.preStart =
+        let
+          adminCmd = "${lib.getExe config.services.forgejo.package} admin user";
+          pwd = config.sops.secrets."forgejo/admin-user-password";
+          user = "britter"; # Note, Forgejo doesn't allow creation of an account named "admin"
+        in
+        ''
+          ${adminCmd} create --admin --email "root@localhost" --username ${user} --password "$(tr -d '\n' < ${pwd.path})" || true
+          ## uncomment this line to change an admin user which was already created
+          # ${adminCmd} change-password --username ${user} --password "$(tr -d '\n' < ${pwd.path})" || true
+        '';
 
       services.https-proxy = {
         enable = true;
