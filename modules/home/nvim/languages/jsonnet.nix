06ef2b57384c262574893ae1f4756e735ee2ff49
Benedikt Ritter <beneritter@gmail.com>
2026-07-15 16:31:15 +0200

    nvim: add jsonnetfmt formatting source

---
+++ modules/home/nvim/languages/jsonnet.nix
@@ -0,0 +1,25 @@
+_: {
+  flake.modules.homeManager.nvim =
+    { pkgs, ... }:
+    {
+      programs.nixvim.plugins.none-ls.luaConfig.post =
+        # lua
+        ''
+          do
+            local null_ls = require("null-ls")
+            local helpers = require("null-ls.helpers")
+
+            null_ls.register({
+              method = null_ls.methods.FORMATTING,
+              filetypes = { "jsonnet" },
+              name = "jsonnetfmt",
+              generator = helpers.formatter_factory({
+                command = "${pkgs.go-jsonnet}/bin/jsonnetfmt",
+                args = { "-" },
+                to_stdin = true,
+              }),
+            })
+          end
+        '';
+    };
+}
