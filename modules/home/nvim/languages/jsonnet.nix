dd13b6290530ae5d18fe90f890f9dc059d38c329
Benedikt Ritter <beneritter@gmail.com>
2026-07-16 16:38:12 +0200

    nvim: enable jsonnet lsp

--- modules/home/nvim/languages/jsonnet.nix
+++ modules/home/nvim/languages/jsonnet.nix
@@ -2,24 +2,27 @@ _: {
   flake.modules.homeManager.nvim =
     { pkgs, ... }:
     {
-      programs.nixvim.plugins.none-ls.luaConfig.post =
-        # lua
-        ''
-          do
-            local null_ls = require("null-ls")
-            local helpers = require("null-ls.helpers")
+      programs.nixvim.plugins = {
+        lsp.servers.jsonnet_ls.enable = true;
+        none-ls.luaConfig.post =
+          # lua
+          ''
+            do
+              local null_ls = require("null-ls")
+              local helpers = require("null-ls.helpers")
 
-            null_ls.register({
+              null_ls.register({
               method = null_ls.methods.FORMATTING,
               filetypes = { "jsonnet" },
               name = "jsonnetfmt",
               generator = helpers.formatter_factory({
-                command = "${pkgs.go-jsonnet}/bin/jsonnetfmt",
-                args = { "-" },
-                to_stdin = true,
-              }),
-            })
-          end
-        '';
+              command = "${pkgs.go-jsonnet}/bin/jsonnetfmt",
+                  args = { "-" },
+                  to_stdin = true,
+                }),
+              })
+            end
+          '';
+      };
     };
 }
