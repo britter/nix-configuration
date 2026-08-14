799bade874132a06debe8838df09e43fd0b81e4a
Benedikt Ritter <beneritter@gmail.com>
2026-08-14 15:39:29 +0200

    nvim: add nvim-pqf for nicer quickfix list

--- modules/home/nvim/ui.nix
+++ modules/home/nvim/ui.nix
@@ -1,16 +1,24 @@
 _: {
-  flake.modules.homeManager.nvim = {
-    programs.nixvim.plugins = {
-      dropbar.enable = true;
-      lualine.enable = true;
-      indent-blankline = {
-        enable = true;
-        settings.scope = {
-          show_start = false;
-          show_end = false;
+  flake.modules.homeManager.nvim = { pkgs, ... }: {
+    programs.nixvim = {
+      plugins = {
+        dropbar.enable = true;
+        lualine.enable = true;
+        indent-blankline = {
+          enable = true;
+          settings.scope = {
+            show_start = false;
+            show_end = false;
+          };
         };
+        web-devicons.enable = true;
       };
-      web-devicons.enable = true;
+      extraPlugins = [ pkgs.vimPlugins.nvim-pqf ];
+      extraConfigLua =
+        #lua
+        ''
+          require('pqf').setup()
+        '';
     };
   };
 }
