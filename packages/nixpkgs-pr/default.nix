1a80537beb466d3bdd6ddeb7d640c2a6e29ebed2
Benedikt Ritter <beneritter@gmail.com>
2026-08-26 10:08:38 +0200

    nixpkgs-pr: update to latest commit

--- packages/nixpkgs-pr/default.nix
+++ packages/nixpkgs-pr/default.nix
@@ -12,11 +12,11 @@ rustPlatform.buildRustPackage (_finalAttrs: {
   src = fetchFromCodeberg {
     owner = "britter";
     repo = "nixpkgs-pr";
-    rev = "c9164fc391380ca0d259c5f9474b3a4e6ac16c08";
-    hash = "sha256-eCJLTtiFDB+t54PSd3d7mMwPgExkZMTskwqiFhA93rc=";
+    rev = "4677cabf029fe552e9af3f91b6a91b3c64db8b14";
+    hash = "sha256-PqIMD1rY8n/o5AD0F11xV8FFUHYexAw25Po0LMlC2YQ=";
   };
 
-  cargoHash = "sha256-0q95j+sO7pAgV4XPO2UNS6HnOIW5wVOxCkCbVnyAK18=";
+  cargoHash = "sha256-Jhy0nldP778ROAR1Gz7kuHWN5cZXm+S0nbl0B+p/lOE=";
 
   doInstallCheck = true;
   nativeInstallCheckInputs = [ versionCheckHook ];
