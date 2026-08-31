02e6299507cc29c8d25213751d0fc64652c186e0
Benedikt Ritter <beneritter@gmail.com>
2026-08-31 14:16:59 +0200

    nixpkgs-pr: update to latest commit

--- packages/nixpkgs-pr/default.nix
+++ packages/nixpkgs-pr/default.nix
@@ -12,8 +12,8 @@ rustPlatform.buildRustPackage (_finalAttrs: {
   src = fetchFromCodeberg {
     owner = "britter";
     repo = "nixpkgs-pr";
-    rev = "4677cabf029fe552e9af3f91b6a91b3c64db8b14";
-    hash = "sha256-PqIMD1rY8n/o5AD0F11xV8FFUHYexAw25Po0LMlC2YQ=";
+    rev = "7ec52918f5120185e20c57f8c1130f41a557e802";
+    hash = "sha256-yVQBbYbNcHO6l5UmNtIqAjDyd2BHip8yI8EqCAc1Z9Q=";
   };
 
   cargoHash = "sha256-Jhy0nldP778ROAR1Gz7kuHWN5cZXm+S0nbl0B+p/lOE=";
