41cdf6e0c7b393c41d06db0e3690886bd1fa071a
Benedikt Ritter <beneritter@gmail.com>
2026-09-14 09:55:11 +0200

    kotlin-lsp: update to latest release

--- packages/kotlin-lsp/default.nix
+++ packages/kotlin-lsp/default.nix
@@ -8,11 +8,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "kotlin-lsp";
-  version = "263.4421.0";
+  version = "263.4702.0";
 
   src = fetchzip {
     url = "https://download-cdn.jetbrains.com/language-server/kotlin-server/${finalAttrs.version}/kotlin-server-${finalAttrs.version}.tar.gz";
-    sha256 = "sha256-NI5hgLvganNwYP30fpeETpY5MLtvOOgpAjoNfyd1knA=";
+    sha256 = "sha256-v7afkhXixaB2nSqhuyMhr0z5/biodUl5uuHJyMoBciQ=";
   };
 
   nativeBuildInputs = [
