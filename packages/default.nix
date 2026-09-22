ba4a4c08204e4a0f2b1541d94283c6f1c030dacf
Benedikt Ritter <beneritter@gmail.com>
2026-09-22 15:21:18 +0200

    groovy-lsp: remove package
    It's now available in nixpkgs

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,6 @@
 {
   clipmd = pkgs.callPackage ./clipmd { };
   gh-get = pkgs.callPackage ./gh-get { };
-  groovy-language-server = pkgs.callPackage ./groovy-language-server { };
   jfmt-java = pkgs.callPackage ./jfmt-java { };
   kotlin-lsp = pkgs.callPackage ./kotlin-lsp { };
   nixpkgs-pr = pkgs.callPackage ./nixpkgs-pr { };
